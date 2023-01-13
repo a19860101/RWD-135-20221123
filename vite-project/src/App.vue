04b34f657b2f681bd9c663e9a7fbff76e41f7e51
Zac <a19860101@gmail.com>
2023-01-13 20:28:11 +0800

    20230113

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,6 +1,7 @@
-<template>
-  <h1>HELLO</h1>
-</template>
 <script>
 
 </script>
+<template>
+  <h1 class="bg-amber-300 p-4 text-4xl font-bold">hello vite</h1>
+  <a href="#" class="btn">MORE</a>
+</template>
