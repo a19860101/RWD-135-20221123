fa02d62b22a6a9acc07520cb2f32497e78821cfa
Zac <a19860101@gmail.com>
2023-01-13 21:48:35 +0800

    20230113

--- vite-project/src/components/Todos.vue
+++ vite-project/src/components/Todos.vue
@@ -27,6 +27,14 @@ export default {
             });
             this.input = '';
         }
+    },
+    computed:{
+        todoComplete(){
+            return this.lists.filter(data => data.isCompleted)
+        },
+        todoUnComplete(){
+            return this.lists.filter(data => !data.isCompleted)
+        }
     }
 }
 </script>
@@ -37,14 +45,26 @@ export default {
             <input type="text" v-model="input" class="border border-teal-600 rounded-lg p-2 mr-3">
             <input type="submit" class="btn" @click="add()">
         </form>
+        <h2 v-show="todoUnComplete.length > 0">未完成</h2>
         <ul class="w-[400px] space-y-3">
-            <li v-for="list in lists" :key="list.id" class="w-full flex bg-teal-200 p-3 rounded-lg">
+            <li v-for="list in todoUnComplete" :key="list.id" class="w-full flex bg-teal-200 p-3 rounded-lg">
                 <div class="grow">{{list.name}}</div>
                 <form action="" class="w-5 text-center">
                     <input type="checkbox" v-model="list.isCompleted">    
                 </form>
             </li>
         </ul>
+        <div class="py-3"></div>
+        <h2 v-show="todoComplete.length > 0">已完成</h2>
+        <ul class="w-[400px] space-y-3">
+            <li v-for="list in todoComplete" :key="list.id" class="w-full flex bg-teal-200 p-3 rounded-lg">
+                <div class="grow">{{list.name}}</div>
+                <form action="" class="w-5 text-center">
+                    <input type="checkbox" v-model="list.isCompleted">    
+                </form>
+            </li>
+        </ul>
+       
     </div>
 
 </template>
