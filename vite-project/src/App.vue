51c41a12743313998908f9f96e952feca0ee47ad
Zac <a19860101@gmail.com>
2023-01-13 21:20:12 +0800

    20230113

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,26 +1,14 @@
 <script>
+  import Todos from './components/Todos.vue'
   export default {
-    data(){
-      return {
-        title: 'Hello VIte'
-      }
-    },
-    methods:{
-      test(){
-        alert('hello vite');
-      }
-    },
-    computed:{
-      hello(){
-        return 'hello';
-      }
+    components:{
+      // Todos:Todos
+      Todos
     }
   }
 </script>
 <template>
   <div>
-    <h1 class="text-4xl mb-3">{{title}}</h1>
-    <h2 class="text-5xl p-4 mb-3 bg-purple-600">{{hello}}</h2>
-    <a href="#" @click="test()" class="btn">click</a>
+    <Todos></Todos>
   </div>
 </template>
---
+++ vite-project/src/components/Todos.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    data(){
+        return {
+            title: 'Todo List',
+            lists:[
+                {
+                    id:1,
+                    name:'寄信',
+                    isCompleted: true
+                },
+                {
+                    id:2,
+                    name:'洗碗',
+                    isCompleted: false
+                }
+            ]
+        }
+    }
+}
+</script>
+<template>
+    <div class="p-3">
+        <h1 class="text-4xl font-bold">{{title}}</h1>
+        <ul>
+            <li v-for="list in lists" :key="list.id">
+                {{list.name}}
+                <input type="checkbox" v-model="list.isCompleted">    
+            </li>
+        </ul>
+    </div>
+
+</template>
