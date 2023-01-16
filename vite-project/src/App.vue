fa354466382899cef5029a7ff19dc5811d5ef6ec
Zac <a19860101@gmail.com>
2023-01-16 20:13:46 +0800

    20230116

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,14 +1,18 @@
 <script>
-  import Todos from './components/Todos.vue'
+  // import Todos from './components/Todos.vue'
+  import Nav from './components/Nav.vue'
   export default {
     components:{
       // Todos:Todos
-      Todos
+      // Todos
+      Nav
     }
   }
 </script>
 <template>
   <div>
-    <Todos></Todos>
+    <!-- <Todos></Todos>。 -->
+    <h1 class="text-3xl font-bold bg-lime-300 p-4">My Vite</h1>
+    <Nav></Nav>
   </div>
 </template>
---
+++ vite-project/src/components/Nav.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <nav class="bg-rose-500 p-4 text-center space-x-3">
+        <router-link to="/">Home</router-link>
+        <router-link to="/">About</router-link>
+        <router-link to="/">Service</router-link>
+        <router-link to="/">Contact</router-link>
+    </nav>
+</template>
